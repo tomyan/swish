605f62fe8711127cf78b0fbefef8698e6bb72a1a
Thomas Yandell <thomasyandell@mac.config>
2010-02-28 14:02:27 +0000

    Initial cut at history and history navigation with arrow keys.

--- src/js/shell.js
+++ src/js/shell.js
@@ -42,15 +42,24 @@ soda.module({
         };
 
         View.prototype.initEventHandlers = function (controller) {
-            bind( 'keyup', controller.onkeyup, controller);
+            bind(this.commandInput, 'keydown', controller.onkeydown, controller);
+            bind(this.commandInput, 'keyup', controller.onkeyup, controller);
             bind(this.commandInput, 'keypress', controller.onkeypress, controller);
             bind(this.form, 'submit', function () { return false; });
             var view = this;
             bind(window, 'focus', function () { view.focusInput(); });
         };
 
+        View.prototype.currentCommand = function () {
+            return this.commandInput.val();
+        };
+
+        View.prototype.setCurrentCommand = function (command) {
+            this.commandInput.val(command);
+        };
+
         View.prototype.getCommand = function () {
-            var command = this.commandInput.val();
+            var command = this.currentCommand();
             var previous = this.lastCommandListItem = create('<li></li>');
             previous.text(command); 
             this.formListItem.before(previous);
@@ -72,10 +81,6 @@ soda.module({
             this.focusInput();
         };
 
-        var Model = function () {
-
-        };
-
         var Command = function (func) {
             this.func = func;
         };
@@ -88,8 +93,11 @@ soda.module({
             this.view = new View();
             this.view.init('body');
             this.view.initEventHandlers(this);
-
-            this.model = new Model();
+            this.history = [];
+            // this is a sparse array of modified versions of the history that is overwritten after
+            // every command
+            this.currentHistory = [];
+            this.currentHistoryPosition = 0;
         };
 
         var builtins = {};
@@ -99,12 +107,14 @@ soda.module({
         });
 
         var keys = {
-            'ENTER' : 'processCommand',
-            'CTRL+l' : 'clearScreen'
+            'press=ENTER'  : 'processCommand',
+            'press=CTRL+l' : 'clearScreen',
+            'down=UP'      : 'previousCommand',
+            'down=DOWN'    : 'nextCommand'
         };
 
         Controller.prototype.parseCommand = function (command) {
-            // TODO lots
+            // TODO lots of parsing
             return command.split(/\s+/);
         };
 
@@ -113,8 +123,43 @@ soda.module({
             return false;
         };
 
+        Controller.prototype.previousCommand = function () {
+            if (this.currentHistoryPosition != 0) {
+                // save the current edits
+                this.currentHistory[this.currentHistoryPosition] = this.view.currentCommand();
+                this.currentHistoryPosition--;
+                this.view.setCurrentCommand(
+                    typeof(this.currentHistory[this.currentHistoryPosition]) == 'undefined' ?
+                        this.history[this.currentHistoryPosition] :
+                        this.currentHistory[this.currentHistoryPosition]
+                );
+            }
+            return false;
+        };
+
+        Controller.prototype.nextCommand = function () {
+            if (this.currentHistoryPosition != this.history.length) {
+                // save the current edits
+                this.currentHistory[this.currentHistoryPosition] = this.view.currentCommand();
+                this.currentHistoryPosition++;
+                this.view.setCurrentCommand(
+                    typeof(this.currentHistory[this.currentHistoryPosition]) == 'undefined' ?
+                        this.history[this.currentHistoryPosition] :
+                        this.currentHistory[this.currentHistoryPosition]
+                );
+            }
+            return false;
+        };
+
         Controller.prototype.processCommand = function () {
-            var args = this.parseCommand(this.view.getCommand()),
+            var plain = this.view.getCommand();
+
+            // TODO history expansion before adding command to history
+            this.history.push(plain);
+            this.currentHistory = [];
+            this.currentHistoryPosition = this.history.length;
+
+            var args = this.parseCommand(plain),
                 name = args.shift();
             if (name == '') return;
             var command;
@@ -129,23 +174,35 @@ soda.module({
             return false;
         };
 
-        Controller.prototype.keyStringFromEvent = function (e) {
+        Controller.prototype.keyStringFromEvent = function (eventType, e) {
             var modifiers = (e.ctrlKey ? 'CTRL+' : ''),
                 keyName = (e.key || e.chr);
+            // work around for FF (don't really know who is right, but doing it this way around)
+            // detect sending of keypress for UP and DOWN and treat as keydown's to match Chrome
+            if ((keyName == 'UP' || keyName == 'DOWN') && eventType == 'press') {
+                eventType = 'down';
+            }
+
             // work around Glow/Chrome bug for CTRL+l
             if (! keyName && e.keyCode == 12) {
                 keyName = 'l';
             }
-            return modifiers + keyName;
+            return eventType + '=' + modifiers + keyName;
         };
 
-        Controller.prototype.onkeypress = function (e) {
-            var keyString = this.keyStringFromEvent(e);
-            console.log('key pressed: ' + keyString);
-            if (keys[keyString]) {
-                return this[keys[keyString]].call(this);
-            }
-        };
+        function keyHandler (eventType) {
+            return function (e) {
+                var keyString = this.keyStringFromEvent(eventType, e);
+                //console.log('key ' + eventType + ': ' + keyString);
+                if (keys[keyString]) {
+                    return this[keys[keyString]].call(this);
+                }
+            };
+        }
+
+        Controller.prototype.onkeypress = keyHandler('press');
+        Controller.prototype.onkeyup    = keyHandler('up');
+        Controller.prototype.onkeydown  = keyHandler('down');
 
         ns.init = function () {
             var controller = new Controller();
